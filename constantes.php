3446b4ad1a8c1e36e37a3116171eb1fea5dba1e0
Mateyro <mateyro999@gmail.com>
2025-03-27 15:30:35 -0300

    Creado

---
+++ constantes.php
@@ -0,0 +1,38 @@
+<?php 
+define('GRAVEYARD', 9.8);
+const GRAVEYARD = 9.8; // Constantes que representan un valor unico que no cambia durante la ejecucion del script. Espacios en la memoria, no variables.
+
+// 1
+const PI = 3.1416;
+echo PI;
+//2
+const PI = 2.1416;
+echo PI;
+//3
+// Error: Warning: Constant PI already defined in C:\xampp\htdocs\programacionweb2025\constantes.php on line 9
+// Dice que: El constante Pi ya fue definido en la linea 9 en este mismo archivo.
+// Cuando se necesita usar un dato especifico una cantidad de veces una y otra vez ya que es memoria y no una variable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
