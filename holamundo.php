0d1e2af4b10e80ca4e59a3c32b525964eef5fee4
Mateyro <mateyro999@gmail.com>
2025-03-27 14:41:53 -0300

    Trabajamos con variables

--- holamundo.php
+++ holamundo.php
@@ -1,1 +1,30 @@
-<?php echo "Hola Mundo!"; ?> 
+<?php echo "Hola Mundo!";//echo test or smthn idk im bored and just typing stuff cuz it feels useful but it isnt hi :P ?> 
+<?php $edad = 18; // Enteros, Int ?>
+<?php $nombre = 'Manolo'; // Texto, String ?>
+<?php $altura = 1.72; // Decimales, Float ?>
+<?php $mayorEdad = true; // Verdadero o Falso, Boolean ?>
+
+<?php $nombreCompleto = "Juan perez";
+// El nombre completo de una persona. Le falta el ; ?>
+
+<?php $Edad = 25;
+// La edad de una persona, su edad es 25. Probablemente se use en algun tipo de database.?>
+
+<?php $estAltura = 1.75;
+// "est" no tiene una descripcion exacta pero probablemente sea "estimada" completando la palabra para que sea "Altura estimada", en cualquier caso y lo que signifique 
+// es una altura de un tipo conectada al nombre de algo, juzgando por el resto de las variables, una persona. ?>
+
+<?php $numero = "10";
+// "Numero" no es nada especifico, ni es un numero, es un String, no tendria ni idea para que se usa esto excepto a lo mejor algun display si se supone que siquiera sea un String
+// es mas probable aun asi que se halla equivocado de tipo de variable. Es imposible saber que hace numero sin mirar a su funcion. ?>
+
+<?php Apellido = 'Fernandez'; // 
+// Le falta el $ para inicializar la variable, despues de eso es claramente el apellido de una persona, no hay otro significado a la palabra despues de todo. ?>
+
+<?php pais_de_origen = 'Uruguay' //
+// De vuelta, le falta el $ para inicializar la variable, y le falta el ; despues de eso es el pais de origen de o una persona o un producto, posiblemente un animal, pero mas probablemente una
+// persona basandose en el resto de las variables que estan listadas. ?>
+
+<?php $salario = $2000; // 
+// Le faltan las '', siendo simplemente $2000, el cual no es posible, es posible que hallan querido decir "$2000" o simplemente "2000" dpendiendo de como funcione el codigo y si 
+// ese numero se necesita usar para algun tipo de matematica en vez de simple display. De otra forma, es obviamente el salario de una persona, el nombre no deja mucho a imaginar ?>
