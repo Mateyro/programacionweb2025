0f2a007a22792d727e61013b5a2affd46a89584e
Mateyro <materyo999@gmail.com>
2025-04-03 14:18:00 -0300

    Trabajamos con Operadores y Concatenacion

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<?php
+
+// = Asignacion
+// += Suma y Asignacion
+// -= Resta y Asignacion
+// *= Multiplicacion y Asignacion
+// /= Division y Asignacion
+// %= Modulo y Asignacion
+// .=Concatenacion y Asignacion
+
+$Edad=1;
+$Edad+= 1;
+echo $Edad;
+
+$A=1;
+$A*=100;
+echo $A;
+
+$B="Hola ";
+$B.= "Bruno";
+echo $B;
+
+
+?>
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php 
+$emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora";
+
+echo 'Me gusta escuchar $emisora';
+// "" Printea cosas como variables combinadas con el texto, cambiando $emisora a Radio Disney.
+// '' Printea el texto puro, manteniendo $emisora como ese texto puro sin contar la variable.
+
+//COMILLAS
+
+$nombre= "Bruno";
+$saludo= "Hola ";
+echo $saludo.$nombre;
+// "." se usa para combinar variables
+?>
