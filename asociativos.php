c88fb6381e03afeb8df030588e288eb0a497deb3
Mateo <mateyro999@gmail.com>
2025-08-07 15:41:33 -0300

    act 5 - 8

--- asociativos.php
+++ asociativos.php
@@ -46,21 +46,21 @@ $inventario = [
 $inventario["0"] = [
     "consola" => "NES",
     "año" => "1986",
-    "precio"=> "50.00",
+    "precio"=> "50",
     "stock"=> "10",
     "venta"=> "Si",
 ];
 $inventario["1"] = [
     "consola" => "NES",
     "año" => "1988",
-    "precio"=> "45.00",
+    "precio"=> "45",
     "stock"=> "15",
     "venta"=> "Si",
 ];
 $inventario["2"] = [
     "consola" => "SNES",
     "año" => "1995",
-    "precio"=> "80.00",
+    "precio"=> "80",
     "stock"=> "5",
     "venta"=> "Si",
 ];
@@ -77,7 +77,113 @@ foreach($inventario as $x){
     print("Stock: ".$inventario[$x]["stock"]."<br>");
     print("En venta?: ".$inventario[$x]["venta"]."<br>");
     print("<br>");
+}
 
+print("<br>Actividad 5: <br><br>");
+//act 5
+
+foreach($inventario as $x){
+    if ($x > array_key_last($inventario)) {
+        break;
+    }
+    $keys = array_keys($inventario);
+    print("Titulo: ".$keys[$x]."<br>");
+    print("Consola: ".$inventario[$x]["consola"]."<br>");
+    print("<br>");
+}
+
+print("<br>Actividad 6: <br><br>");
+//act 6
+
+foreach($inventario as $x){
+    if ($x > array_key_last($inventario)) {
+        break;
+    }
+    if ($inventario[$x]["consola"] == "NES") {
+        $inventario[$x]["precio"] = doubleval($inventario[$x]["precio"])-doubleval($inventario[$x]["precio"])*(10/100);
+    }
+}
+
+foreach($inventario as $x){
+    if ($x > array_key_last($inventario)) {
+        break;
+    }
+    $keys = array_keys($inventario);
+    print("Titulo: ".$keys[$x]."<br>");
+    print("Consola: ".$inventario[$x]["consola"]."<br>");
+    print("Año: ".$inventario[$x]["año"]."<br>");
+    print("Precio: ".$inventario[$x]["precio"]."<br>");
+    print("Stock: ".$inventario[$x]["stock"]."<br>");
+    print("En venta?: ".$inventario[$x]["venta"]."<br>");
+    print("<br>");
+}
+
+print("<br>Actividad 7: <br><br>");
+//act 7
+
+$busqueda = "Chrono Trigger";
+print("Juego buscado: ".$busqueda."<br><br>");
+
+foreach($inventario as $x){
+    if ($x > array_key_last($inventario)) {
+        break;
+    }
+    $encontrado = false;
+    $keys = array_keys($inventario);
+    if ($keys[$x] == $busqueda) {
+        $encontrado = true;
+        print("Juego encontrado. <br><br>");
+        print("Titulo: ".$keys[$x]."<br>");
+        print("Consola: ".$inventario[$x]["consola"]."<br>");
+        print("Año: ".$inventario[$x]["año"]."<br>");
+        print("Precio: ".$inventario[$x]["precio"]."<br>");
+        print("Stock: ".$inventario[$x]["stock"]."<br>");
+        print("En venta?: ".$inventario[$x]["venta"]."<br>");
+        print("<br>"); 
+    }
+}
+if ($encontrado == false) {
+    print("Juego no encontrado. <br><br>");
+}
+
+print("<br>Actividad 8: <br><br>");
+//act 8
+
+$newgame = "Sonic the Hedgehog"; // vos escribirias el juego ingresado.
+$keyadd = array_key_last($inventario)+1;
+$newgamekey = $keyadd;
+$newElements = array(
+    $newgame => $newgamekey,
+);
+
+$inventario = array_merge($inventario, $newElements);
+
+$añadirconsola = "Sega Genesis"; // vos ingresarias todo esto.
+$añadiraño = "1991";
+$añadirprecio = "55";
+$añadirstock = "5";
+$añadirventa = "Si";
+
+$inventario[$keyadd] = [
+    "consola" => $añadirconsola,
+    "año" => $añadiraño,
+    "precio"=> $añadirprecio,
+    "stock"=> $añadirstock,
+    "venta"=> $añadirventa,
+];
+
+foreach($inventario as $x){
+    if ($x > array_key_last($inventario)) {
+        break;
+    }
+    $keys = array_keys($inventario);
+    print("Titulo: ".$keys[$x]."<br>");
+    print("Consola: ".$inventario[$x]["consola"]."<br>");
+    print("Año: ".$inventario[$x]["año"]."<br>");
+    print("Precio: ".$inventario[$x]["precio"]."<br>");
+    print("Stock: ".$inventario[$x]["stock"]."<br>");
+    print("En venta?: ".$inventario[$x]["venta"]."<br>");
+    print("<br>");
 }
 
 ?>
